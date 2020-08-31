832e05215b2378fb8899bbcc2a242bedc0db5701
Volodymyr Ostapiuk <Volodymyr_Ostapiuk@epam.com>
2020-08-31 18:36:11 +0300

    Renamed class

--- src/main/java/com/epam/utils/PropertyGetter.java
+++ src/main/java/com/epam/utils/PropertyGetter.java
@@ -5,12 +5,12 @@ import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
 
-public class PropertyGetter {
+public class PropertySource {
     private static final String PROPERTY_PATH = "config.properties";
 
     public static String getProperty(String key) {
         Properties properties = new Properties();
-        try (InputStream inputStream = PropertyGetter.class.getClassLoader().getResourceAsStream(PROPERTY_PATH)) {
+        try (InputStream inputStream = PropertySource.class.getClassLoader().getResourceAsStream(PROPERTY_PATH)) {
             properties.load(Objects.requireNonNull(inputStream));
             return properties.getProperty(key);
         } catch (IOException e) {
