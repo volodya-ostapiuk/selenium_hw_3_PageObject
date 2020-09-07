77a5dffdc7edef69307dd4538e7f303c3f0facb8
Volodymyr Ostapiuk <Volodymyr_Ostapiuk@epam.com>
2020-09-07 17:48:26 +0300

    Added wrapper factory

--- src/main/java/com/epam/pages/BasePage.java
+++ src/main/java/com/epam/pages/BasePage.java
@@ -1,6 +1,6 @@
 package com.epam.pages;
 
-import com.epam.decorator.CustomFieldDecorator;
+import com.epam.decorator.wrapper.CustomFieldDecorator;
 import com.epam.utils.providers.DriverProvider;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
